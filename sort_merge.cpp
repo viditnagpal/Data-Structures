4a42b76e91f1f2cf9eb45b2267e267ca5fa9cda1
Vidit Nagpal <nagpalvidit1@gmail.com>
2020-10-10 17:14:22 +0530

    sort_merge
    merge sort_merge

---
+++ sort_merge.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+merge(int a[], int n, int l, int b[]){
+	int q,s,r,j;
+	q=n/2*l;
+	s=2*l*q;
+	r=n-s;
+	
+	for(i=1;i<=q;i++){
+		lb=
+	}
+}
+
+
+int main(){
+	int i,l,n,t,a[100],p;
+	cin>>n;
+	for(i=0;i<n;i++){
+		cin>>a[i];
+	}
+	for(i=0;i<n;i++){
+		cin>>b[i];
+	}
+	
+	merge(a,n,l,b);
+	
+}
+
+
